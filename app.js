fa3086aa9956ab169a89c5002f5b12af345ac69a
Eddy Caldas <eddycaldas@live.com>
2018-01-06 21:18:56 -0700

    window-methods

---
+++ app.js
@@ -0,0 +1,16 @@
+// Window methods / objects / properties
+
+let val;
+// outer height and width
+val1 = window.outerHeight;
+val2 = window.outerWidth;
+console.log(val1);
+console.log(val2);
+
+// inner height and width
+val3 = window.innerHeight;
+val4 = window.innerWidth;
+console.log(val3)
+console.log(val4)
+
+
